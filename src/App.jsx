5b4733ca07df558b0339bda6060bf1599720face
Ridhima Sharma <sharmaridhima0028@gmail.com>
2026-06-11 12:17:28 +0530

    fix navigate import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Navbar from './components/Navbar/Navbar'
-import { Routes, Route, Link} from 'react-router-dom'
+import { Routes, Route, Navigate} from 'react-router-dom'
 import Orders from './components/pages/Orders/Orders'
 import Add from './components/pages/Add/Add'
 import List from './components/pages/List/List'
@@ -32,3 +32,7 @@ const App = () => {
 }
 
 export default App
+
+
+
+
